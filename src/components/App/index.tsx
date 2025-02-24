3f5595b1a5882514111f29bdd13ed957210cf7b1
Евгений Готлиб <kubaruba.mix@yandex.by>
2025-02-25 00:35:15 +0300

    fix: fixed problem with git

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,7 @@
-import { Button } from './styled'
-
 export default function App() {
   return (
     <>
-      <h1>Hello</h1>
-      <Button>test</Button>
+      <h1>Weather app</h1>
     </>
   )
 }
