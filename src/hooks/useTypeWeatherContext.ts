d430eeaeafce8bd97743f9db74d45f3076df72ab
Евгений Готлиб <kubaruba.mix@yandex.by>
2025-03-04 14:46:19 +0300

    fix: fixed deploy

--- src/hooks/useTypeWeatherContext.ts
+++
@@ -1,8 +0,0 @@
-import { TypeWeatherContext } from '@src/context/TypeWeatherContext/TypeWeatherContextProvider'
-import { useContext } from 'react'
-
-export function useTypeWeatherContext() {
-  const context = useContext(TypeWeatherContext)
-  if (!context) throw new Error('Use app context within provider!')
-  return context
-}
