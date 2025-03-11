74547e6b864e2f8c12195c14e49433cee20c6cab
Евгений Готлиб <kubaruba.mix@yandex.by>
2025-03-11 23:10:06 +0300

    fix: fixed init city

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -11,6 +11,7 @@ import WeatherDisplay from '@components/WeatherDisplay'
 import WeatherDisplayTime from '@components/WeatherDisplayTime'
 
 import { ContainerApp, WrapperApp } from './styled'
+import { Position } from './types'
 
 export default function App() {
   const { typeWeather } = useWeatherType()
@@ -22,18 +23,30 @@ export default function App() {
     .VITE_CITY_SEARCH_BY_COORDINATE
 
   useEffect(() => {
+    const successGetCurrentCity = async (position: Position) => {
+      const { latitude, longitude } = position.coords
+      const response = await requestAPI(
+        `${CITY_SEARCH_BY_COORDINATE}?lat=${latitude}&lon=${longitude}&type=city&lang=en&limit=1&format=json&apiKey=${AUTOCOMPLETE_CITY_API}`,
+      )
+      const { city } = response.data.results[0]
+      dispatch(
+        getWeatherWeek({ lat: latitude, lon: longitude, cityName: city }),
+      )
+      setLoadingCurrentCity(false)
+    }
+
+    const errorGetCurrentCity = () => {
+      setLoadingCurrentCity(false)
+    }
+
     const fetchCityWeather = async () => {
-      window.navigator.geolocation.getCurrentPosition(async position => {
-        const { latitude, longitude } = position.coords
-        const response = await requestAPI(
-          `${CITY_SEARCH_BY_COORDINATE}?lat=${latitude}&lon=${longitude}&type=city&lang=en&limit=1&format=json&apiKey=${AUTOCOMPLETE_CITY_API}`,
-        )
-        const { city } = response.data.results[0]
-        dispatch(
-          getWeatherWeek({ lat: latitude, lon: longitude, cityName: city }),
-        )
-        setLoadingCurrentCity(false)
-      })
+      window.navigator.geolocation.getCurrentPosition(
+        successGetCurrentCity,
+        errorGetCurrentCity,
+        {
+          timeout: 10000,
+        },
+      )
     }
 
     fetchCityWeather()
